0ebe3d657a608ea914b96ed6598d429093a4337a
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-02-23 15:06:32 +0200

    pabeiiiggghhhhtttttssss...bez drag

--- Assets/script/yes.cs
+++ Assets/script/yes.cs
@@ -1,38 +1,17 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-class yes : MonoBehaviour
-{
-	private Color mouseOverColor = Color.blue;
-	private Color originalColor = Color.yellow;
-	private bool dragging = false;
-	private float distance;
-	private Vector3 starDist;
+class yes : MonoBehaviour{
+	
+	public GameObject slaideris1;
+	public GameObject slaideris2;
+	public GameObject mainAtt;
 
-
-
-
-	void OnMouseDown()
-	{
-		distance = Vector3.Distance(transform.position, Camera.main.transform.position);
-		dragging = true;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		Vector3 rayPoint = ray.GetPoint(distance);
-		starDist = transform.position - rayPoint;
-	}
-
-	void OnMouseUp()
-	{
-		dragging = false;
-	}
-
-	void Update()
-	{
-		if (dragging)
-		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			Vector3 rayPoint = ray.GetPoint(distance);
-			transform.position = rayPoint + starDist;
-		}
+	public void mainiLielum(){
+		float vert = slaideris2.GetComponent<Slider>().value;
+		float vert2 = slaideris1.GetComponent<Slider>().value;
+		mainAtt.transform.localScale = new Vector3 (1F * vert+1, 1F * vert2+1, 1);
 	}
 }
