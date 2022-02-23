5b9e023bc729566362d86b8b66d5261462d8d4b4
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-02-23 12:13:44 +0200

    puse pabeigta

---
+++ Assets/script/teksts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class teksts : MonoBehaviour {
+
+
+
+	public string txt;
+	public string age;
+	public GameObject ievadesLauks;
+	public GameObject tekstaAttelosana;
+	public GameObject ageLauks;
+	public GameObject AgeAttelosana;
+
+	public void uzglabaTxt(){
+		txt = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text>().text = "vards- "+txt;
+		age = ageLauks.GetComponent<Text> ().text;
+		AgeAttelosana.GetComponent<Text>().text = "vecums- "+age;
+	}
+}
